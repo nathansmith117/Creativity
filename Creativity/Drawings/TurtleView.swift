5f6981ff80fc5c5ba66547d44f45467fd5d5c982
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-11-28 09:15:23 -0700

    Added turtle and sun

---
+++ Creativity/Drawings/TurtleView.swift
@@ -0,0 +1,56 @@
+//
+//  TurtleView.swift
+//  Creativity
+//
+//  Created by Smith, Nathanael on 11/28/23.
+//
+
+import SwiftUI
+
+struct TurtleView: View
+{
+    var body: some View
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Turtle : Shape
+{
+    func path(in rect : CGRect) -> Path
+    {
+        var logoPath = Path()
+        
+        logoPath.move(to: CGPoint(x: 10, y: 10))
+        logoPath.addLine(to: CGPoint(x: 50, y: 50))
+        logoPath.addLine(to: CGPoint(x: 10, y: 90))
+        logoPath.closeSubpath()
+        
+        return logoPath
+    }
+}
+
+struct Sun : Shape
+{
+    func path(in rect : CGRect) -> Path
+    {
+        var shinyPath = Path()
+        
+        shinyPath.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        shinyPath.addArc(center: CGPoint(x: rect.maxX, y: rect.minY),
+                         radius: 150,
+                         startAngle: Angle(radians: (Double.pi / 2)),
+                         endAngle: Angle(radians: Double.pi),
+                         clockwise: false)
+        
+        shinyPath.closeSubpath()
+        
+        return shinyPath
+    }
+}
+
+#Preview ("Turtles!")
+{
+    TurtleView()
+}
