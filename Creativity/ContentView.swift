95f32f10decb79479a013d2d6e319ea64e49f858
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-11-28 08:34:42 -0700

    First commit

---
+++ Creativity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Creativity
+//
+//  Created by Smith, Nathanael on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Creativity/CreativityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativityApp.swift
+//  Creativity
+//
+//  Created by Smith, Nathanael on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CreativityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
