16dd755d9d13b53189a193cfae5c514f68f80ec9
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-11-28 08:39:30 -0700

    Formatting and icon

--- Creativity/Resources/CreativityApp.swift
+++ Creativity/Resources/CreativityApp.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 @main
-struct CreativityApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+struct CreativityApp: App
+{
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            CreativityView()
         }
     }
 }
