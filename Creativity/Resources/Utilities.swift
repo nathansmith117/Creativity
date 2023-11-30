17bc36628a4d55aaad12efea4cfe71bf76ed61a3
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-11-30 08:02:45 -0700

    Artifact demo added

---
+++ Creativity/Resources/Utilities.swift
@@ -0,0 +1,12 @@
+//
+//  Utilities.swift
+//  Creativity
+//
+//  Created by Smith, Nathanael on 11/30/23.
+//
+
+import Foundation
+
+let demoArtifact : Artifact = Artifact(filename: "Programming Haiku",
+                                       title: "Programming Haiku!!!", artist: "me",
+                                       description: "A quite wacky programming haiku")
