d0c77d678103973c44276128a427764f333ece88
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-04 08:00:14 -0700

    Added mirroring methods

--- Creativity/Resources/Utilities.swift
+++ Creativity/Resources/Utilities.swift
@@ -5,8 +5,32 @@
 //  Created by Smith, Nathanael on 11/30/23.
 //
 
-import Foundation
+import SwiftUI
 
 let demoArtifact : Artifact = Artifact(filename: "Programming Haiku",
                                        title: "Programming Haiku!!!", artist: "me",
                                        description: "A quite wacky programming haiku")
+
+func virticalMirror(of path : Path, int rect : CGRect) -> Path
+{
+    var mirrored = Path()
+    
+    mirrored.addPath(path)
+    mirrored = mirrored.applying(CGAffineTransform(translationX: -rect.origin.x, y: 0))
+    mirrored = mirrored.applying(CGAffineTransform(scaleX: -1, y: 1))
+    mirrored = mirrored.applying(CGAffineTransform(translationX: rect.origin.x + rect.width, y: 0))
+    
+    return mirrored
+}
+
+func horizontalMirror(of path : Path, int rect : CGRect) -> Path
+{
+    var mirrored = Path()
+    
+    mirrored.addPath(path)
+    mirrored = mirrored.applying(CGAffineTransform(translationX: 0, y: -rect.origin.y))
+    mirrored = mirrored.applying(CGAffineTransform(scaleX: 1, y: -1))
+    mirrored = mirrored.applying(CGAffineTransform(translationX: 0, y: rect.origin.y + rect.height))
+    
+    return mirrored
+}
---
+++ Creativity/ViewController/BobRossView.swift
@@ -0,0 +1,21 @@
+//
+//  BobrossView.swift
+//  Creativity
+//
+//  Created by Smith, Nathanael on 12/4/23.
+//
+
+import SwiftUI
+
+struct BobRossView: View
+{
+    var body: some View
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview ("Bob Ross View")
+{
+    BobRossView()
+}
