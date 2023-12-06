ad2540cd1fbd0a6a114d453725447e53362eac6b
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-06 07:41:12 -0700

    Added rainbow view

---
+++ Creativity/ViewController/RainbowView.swift
@@ -0,0 +1,21 @@
+//
+//  RainbowView.swift
+//  Creativity
+//
+//  Created by Smith, Nathanael on 12/6/23.
+//
+
+import SwiftUI
+
+struct RainbowView: View
+{
+    var body: some View
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview ("Inclusive Gradients!")
+{
+    RainbowView()
+}
