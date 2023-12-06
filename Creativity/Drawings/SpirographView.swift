c046bee735e880e2f449a695a48ae71be2863c28
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-06 08:16:28 -0700

    Using some funny funny trig now

--- Creativity/Drawings/SpirographView.swift
+++ Creativity/Drawings/SpirographView.swift
@@ -17,9 +17,31 @@ struct SpirographView: View
 
 struct Spirograph : Shape
 {
+    var scalar: Double = 20.0
+    var increment: Double = 0.5
+    var repetitions: Double = 360.0
+    var smallRadius: Double = 10
+    var largeRadius: Double = 17
+    
     func path(in rect: CGRect) -> Path
     {
         var niftyPath = Path()
+        
+        var currentX = Double(rect.midX)
+        var currentY = Double(rect.midY)
+        
+        niftyPath.move(to: CGPoint(x: currentX, y: currentY))
+        
+        for theta in stride(from: 0.0, to: repetitions, by: increment)
+        {
+            let deltaX = cos(theta) * (largeRadius - smallRadius) + scalar * cos((smallRadius - largeRadius) * (theta / smallRadius))
+            let deltaY = sin(theta) * (largeRadius - smallRadius) + scalar * sin((smallRadius - largeRadius) * (theta / smallRadius))
+            
+            currentX += deltaX
+            currentY += deltaY
+            
+            niftyPath.addLine(to: CGPoint(x: currentX, y: currentY))
+        }
         
         return niftyPath
     }
