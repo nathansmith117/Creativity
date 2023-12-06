8a559d3cc776ab5f23e23b1429a3da34f6d273d2
Nathan <93161331+nathansmith117@users.noreply.github.com>
2023-12-06 07:49:02 -0700

    Added triangle shape

--- Creativity/ViewController/RainbowView.swift
+++ Creativity/ViewController/RainbowView.swift
@@ -15,6 +15,21 @@ struct RainbowView: View
     }
 }
 
+struct Triangle : Shape
+{
+    func path(in rect : CGRect) -> Path
+    {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX / 3, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
 #Preview ("Inclusive Gradients!")
 {
     RainbowView()
